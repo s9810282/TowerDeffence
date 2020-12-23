c702a498ab264d6cb239fd8aca62a5b42ba88764
choiyonggeun <s9810282@naver.com>
2020-12-23 22:36:31 +0900

    Feat. Simple ObjectPool

---
+++ Assets/03.Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] Transform startPoint;
+    [SerializeField] List<string> monsterList;
+
+    [SerializeField] float spawnDelayTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawnEnemys());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    IEnumerator StartSpawnEnemys()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(spawnDelayTime);
+        
+        while(monsterList.Count > 0)
+        {
+            GameObject monster = ObjectPool.Instance.GetObject();
+            monsterList.RemoveAt(0);
+
+            yield return waitTime;
+        }
+    }
+}
---
+++ Assets/03.Scripts/Tool/ObjectPool.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private static ObjectPool instance;
+    public static ObjectPool Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<ObjectPool>();
+
+            return instance;
+        }
+    }
+
+
+    [SerializeField] GameObject poolingObjectPrefab;
+    [SerializeField] Transform parent;
+    [SerializeField] int count;
+
+
+    Queue<GameObject> poolingObjectQueue = new Queue<GameObject>();
+
+
+
+    private void Awake()
+    {
+        Initialize(count);
+    }
+
+    private void Initialize(int initCount)
+    {
+        for (int i = 0; i < initCount; i++)
+        {
+            poolingObjectQueue.Enqueue(CreateNewObject());
+        }
+    }
+
+    private GameObject CreateNewObject()
+    {
+        GameObject newObj = Instantiate(poolingObjectPrefab);
+        newObj.gameObject.SetActive(false);
+        newObj.transform.SetParent(parent);
+        return newObj;
+    }
+
+
+
+    public GameObject GetObject()
+    {
+        if (Instance.poolingObjectQueue.Count > 0)
+        {
+            var obj = Instance.poolingObjectQueue.Dequeue();
+            obj.transform.SetParent(parent);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newObj = Instance.CreateNewObject();
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(parent);
+            return newObj;
+        }
+    }
+
+
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.gameObject.SetActive(false);
+        obj.transform.SetParent(parent);
+        Instance.poolingObjectQueue.Enqueue(obj);
+    }
+
+}
